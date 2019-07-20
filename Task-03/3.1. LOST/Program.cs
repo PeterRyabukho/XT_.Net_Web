3dc53f9ffd68a29b54531abbdb5a3d94f7a04381
ryabukhopeter.optics <ryabukhopeter.optics@yandex.ru>
2019-07-20 19:03:43 +0400

    Added Task-03! (Ex. 3.1 - 3.2)
    Complite ex 3.1 and 3.2

---
+++ Task-03/3.1. LOST/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3._1._LOST
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] people = CreateCirclOfPeople();
+
+            if (people == null)
+            {
+                throw new NullReferenceException("Введено некотректно значение N массив не был создан!");
+            }
+            
+            Queue<int> peopleInCercl = new Queue<int>(people);
+
+            LostGame(peopleInCercl);
+            Console.WriteLine($"\n\nПобедитель: человек под номером [{peopleInCercl.Peek()}] !");
+
+            Console.ReadLine();
+        }
+
+        private static int[] CreateCirclOfPeople()
+        {
+            Console.Write("Введите количество человек в кругу: ");
+            bool check = int.TryParse(Console.ReadLine(), out int N);
+
+            if (check && N > 1)
+            {
+                int[] people = new int[N];
+                for (int i = 0; i <= people.Length - 1; i++)
+                {
+                    people[i] = i + 1;
+                }
+
+                Console.Write("\nСформирован круг из людей под номерами: ");
+                for (int i = 0; i < people.Length; i++)
+                {
+                    Console.Write(people[i] + " ");
+                }
+                Console.WriteLine();
+                return people;
+            }
+            else
+            {
+                Console.WriteLine("\nОшибка! Количество человек должно быть целое число и большее 1 !!!\n");
+                Main();
+                return null;
+            }
+        }
+
+        private static void LostGame(Queue<int> peopleInCercl)
+        {
+            int countLoops = 1;
+            while (peopleInCercl.Count != 1)
+            {
+                Console.Write($"\nОсталось человеков после {countLoops} круга уничтожения под номерами: ");
+                peopleInCercl.Enqueue(peopleInCercl.Dequeue());
+                peopleInCercl.Dequeue();
+                int[] arr = peopleInCercl.ToArray();
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.Write(arr[i] + " ");
+                }
+                countLoops++;
+            }
+        }
+    }
+}
---
+++ Task-03/3.2. WORD FREQUENCY/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3._2._WORD_FREQUENCY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "The upgrade should provide the functionality to set two properties: the number of orders needed to be eligible for the discount, and the percentage of the discount. " +
+                "This makes it a perfect scenario for default interface members. You can add a method to the ICustomer interface, and provide the most likely implementation. " +
+                "All existing, and any new implementations can use the default implementation, or provide their own";
+
+            string[] arr = str1.ToLower().Split(' ', '.', ',','-') ;
+
+            Dictionary<string, List<int>> myDictionary = new Dictionary<string, List<int>>();
+            int count = 1;
+            foreach (var word in arr)
+            {
+                if(!myDictionary.ContainsKey(word))
+                {
+                    myDictionary.Add(word, new List<int>(count));
+                }
+                else
+                {
+                    myDictionary[word].Add(count++);
+                }
+            }
+
+            Console.WriteLine($"     Слова\t\t\tКол-во повторений в тексте");
+            foreach (var word in myDictionary)
+            {
+
+                Console.WriteLine($"{word.Key, 10}\t\t\t{word.Value.Count+1}");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
