dc5e73672598b53205a8c4c07319924a7eac0988
ryabukhopeter.optics <ryabukhopeter.optics@yandex.ru>
2019-07-26 21:03:28 +0400

    adedd some solution in 4.1 & 4.4

---
+++ Task-04/4.1. CUSTOM SORT/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace _4._1._CUSTOM_SORT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] arr = new double[] { 88, 4, 99, 1, 7 };
+
+            MySort<double> my = new MySort<double>();
+
+            Sort(arr, (arr1) =>
+                 {
+                     for (int i = 0; i < arr.Length; i++)
+                     {
+                         for (int j = i + 1; j < arr.Length; j++)
+                         {
+                             if (arr[i] > arr[j])
+                             {
+                                 var move = arr[j];
+                                 arr[j] = arr[i];
+                                 arr[i] = move;
+                             }
+                         }
+                     }
+                     return arr1;
+                 });    
+
+
+            foreach (var item in arr)
+            {
+                Console.Write(item+" ");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static T[] Sort<T>(T[] arr, Func<T[],T[]> x)
+        {
+            return x(arr);
+        }
+
+            //public static T[] Sort<T>(T[] arr, Func<T[], T, T> comp,T x)
+            //{ 
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    for (int j = i+1; j < arr.Length; j++)
+            //    {
+            //        if(comp(arr[i])>0)
+            //        {
+            //            var move = arr[i];
+            //            arr[i] = arr[j];
+            //            arr[j] = move;
+
+            //        }
+            //    }
+            //}
+            //return arr;
+            //}
+    }
+}
---
+++ Task-04/4.4. NUMBER ARRAY SUM/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _4._4._NUMBER_ARRAY_SUM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetSumm((x, y) => { var sum = x + y; return sum; }, 3.88, 5));
+
+
+            Console.ReadKey();
+        }
+
+        public static T GetSumm<T>(Func<T, T, T> summ, T x, T y)
+        {
+            return summ(x, y);
+        }
+    }
+}
