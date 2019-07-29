6522e99610ef829a64be1ad1906333e19f23715a
Peter Ryabukho <ryabukhopeter.optics@yandex.ru>
2019-07-29 17:33:06 +0400

    Task-04 is Complite!

--- Task-04/4.4. NUMBER ARRAY SUM/Program.cs
+++ Task-04/4.4. NUMBER ARRAY SUM/Program.cs
@@ -6,15 +6,45 @@ namespace _4._4._NUMBER_ARRAY_SUM
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(GetSumm((x, y) => { var sum = x + y; return sum; }, 3.88, 5));
+            Console.Write("Int array created: ");
+            int[] arr1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            ShowMeThisArray(arr1);
 
+            Console.WriteLine($"Sum of all int array elements is: {GetSumm((x,y)=>x+y,arr1)}");
+
+            Console.Write("\nDouble array created: ");
+            double[] arr2 = { 1.6, 2.3, 3.8, 4.2, 5.9, 6.2, 7.1, 8.4, 9.1 };
+            ShowMeThisArray(arr2);
+
+            Console.WriteLine($"Sum of all double array elements is: {GetSumm((x, y) => x + y, arr2)}");
+
+            Console.Write("\nString array created: ");
+            string[] arr3 = { "hi", "my", "name", "is", "Jon"};
+            ShowMeThisArray(arr3);
+
+            Console.WriteLine($"Sum of all double array elements is: {GetSumm((x, y) => x + y, arr3)}");
 
             Console.ReadKey();
         }
 
-        public static T GetSumm<T>(Func<T, T, T> summ, T x, T y)
+        public static T GetSumm<T>(Func<T, T, T> summFunc, T[] arr)
         {
-            return summ(x, y);
+            T multi = default;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                multi = summFunc(multi, arr[i]);
+            }
+
+            return multi;
+        }
+        private static void ShowMeThisArray<T>(T[] arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
         }
     }
 }
---
+++ Task-04/4.6. I SEEK YOU/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4._6._I_SEEK_YOU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Create new array: ");
+            int[] arr = { -1, 4, 44, 7, -3, -77, 5, -876, 435, 43, -38, -2, 211 };
+            ShowMeThisArray(arr);
+
+            Console.Write("\n1. Simple method serching positive: ");
+            var simpleSerch = SimpleFindPositive(arr);
+            ShowMeThisArray(simpleSerch);
+
+            Console.Write("2. Method, to which the search condition is passed through the delegate instance: ");
+            Predicate<int> predicate = new Predicate<int>(GetPositiveIndex);
+            var delegSerch = DelegFindPositive(arr, predicate);
+            ShowMeThisArray(delegSerch);
+
+            Console.Write("3. Method, to which the search condition is passed through the delegate as an anonymous method: ");
+            var anonymSerch = DelegFindPositive(arr,
+                delegate (int i)
+                {
+                    return i > 0;
+                });
+            ShowMeThisArray(anonymSerch);
+
+            Console.Write("4. Method, to which the search condition is passed through the delegate in the form of lambda expression: ");
+            var lambdaSerch = DelegFindPositive(arr, (i) => i > 0);
+            ShowMeThisArray(lambdaSerch);
+
+            Console.Write("4. Method, to which the search condition is passed through LINQ: ");
+            var linqSerch = LinqFindPositive(arr);
+            ShowMeThisArray(linqSerch);
+
+            Console.ReadLine();
+        }
+
+        private static void ShowMeThisArray<T>(T[] arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+        }
+
+        private static bool GetPositiveIndex(int i) => i > 0;
+
+        private static int[] SimpleFindPositive(int[] arr)
+        {
+            List<int> ts = new List<int>();
+
+            foreach (var item in arr)
+            {
+                if (item > 0)
+                {
+                    ts.Add(item);
+                }
+            }
+
+            return ts.ToArray();
+        }
+
+        private static T[] DelegFindPositive<T>(T[] arr, Predicate<T> find)
+        {
+            List<T> ts = new List<T>();
+
+            foreach (var item in arr)
+            {
+                if(find.Invoke(item))
+                {
+                    ts.Add(item);
+                }
+            }
+
+            return ts.ToArray();
+        }
+
+        private static int[] LinqFindPositive(int[] arr)
+        {
+            arr = (from item in arr
+                   where item > 0
+                   select item).ToArray();
+
+            return arr;
+        }
+    }
+}
