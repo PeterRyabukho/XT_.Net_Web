4468908a5d6cce5a14a184fe1370ea7b768ca26a
Peter Ryabukho <ryabukhopeter.optics@yandex.ru>
2019-08-26 12:25:51 +0400

    refactoring Task-08 all done!

--- Task-07/Task-07. REGULAR EXPRESSIONS/7.2. HTML REPLACER/Program.cs
+++ Task-07/Task-07. REGULAR EXPRESSIONS/7.2. HTML REPLACER/Program.cs
@@ -17,11 +17,12 @@ namespace _7._2.HTML_REPLACER
 
         private static void TagsReplacer()
         {
-            Console.WriteLine("1. Start program");
-            Console.WriteLine("2. Exit");
+            Console.WriteLine("1. Start program and use ready HTML5 file");
+            Console.WriteLine("2. Enter tags manually through the console");
+            Console.WriteLine("3. Exit");
 
             bool checkInput = int.TryParse(Console.ReadLine(), out int result);
-            if (checkInput && result > 0 && result < 3)
+            if (checkInput && result > 0 && result < 4)
             {
                 switch (result)
                 {
@@ -35,13 +36,7 @@ namespace _7._2.HTML_REPLACER
                         Console.WriteLine("\nPress any button to replace all tags!");
                         Console.ReadLine();
 
-                        Regex tagFinder = new Regex(@"<.+?>");
-                        var matches = tagFinder.Matches(inputHtmlFile);
-
-                        foreach (Match item in matches)
-                        {
-                            inputHtmlFile = inputHtmlFile.Replace(item.ToString(), "_");
-                        }
+                        inputHtmlFile = AllTagsFinder(inputHtmlFile);
 
                         using (StreamWriter streamWriter = new StreamWriter(@"D:\GitApp\XT_.Net_Web\Task-07\Task-07. REGULAR EXPRESSIONS\7.2. HTML REPLACER\HTML file with tags.html"))
                         {
@@ -51,7 +46,7 @@ namespace _7._2.HTML_REPLACER
                         Console.WriteLine("\nSuccessfully! Tags replaced by underscore!\n");
 
                         string replacedHtmlFile = ReadMyFaile();
-                        Console.WriteLine("\nY'r Replaced file!\n");
+                        Console.WriteLine("\nIn Y'r file Tags successfully replaced by underscores!\n");
                         Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
                         Console.WriteLine(replacedHtmlFile);
                         Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
@@ -61,6 +56,17 @@ namespace _7._2.HTML_REPLACER
                         TagsReplacer();
                         break;
                     case 2:
+                        Console.WriteLine("Enter tex with tags according to the given pattern - '<tag> text text <\\tag> text <tag style=\"tag style\">' etc: ");
+                        Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
+                        string inputTags = Console.ReadLine();
+                        Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
+                        inputTags = AllTagsFinder(inputTags);
+                        Console.WriteLine("\nY'r Tags replaced by underscores!\n");
+                        Console.WriteLine(inputTags);
+                        Console.ReadLine();
+                        TagsReplacer();
+                        break;
+                    case 3:
                         return;
                 }
             }
@@ -70,6 +76,19 @@ namespace _7._2.HTML_REPLACER
                 Console.ReadKey();
                 TagsReplacer();
             }
+        }
+
+        private static string AllTagsFinder(string inputHtmlFile)
+        {
+            Regex tagFinder = new Regex(@"<.+?>");
+            var matches = tagFinder.Matches(inputHtmlFile);
+
+            foreach (Match item in matches)
+            {
+                inputHtmlFile = inputHtmlFile.Replace(item.ToString(), "_");
+            }
+
+            return inputHtmlFile;
         }
 
         private static string ReadMyFaile()
