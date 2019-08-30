6d9b35ed26a1cddff1a89a552642e3acca57bddd
ryabukhopeter.optics <ryabukhopeter.optics@yandex.ru>
2019-08-30 20:02:20 +0400

    compliete 9.3

--- Task-09/9.3. BUTTERFLY CONTROL/9.3 script.js
+++ Task-09/9.3. BUTTERFLY CONTROL/9.3 script.js
@@ -1,41 +1,37 @@
-function onClick_fromAvToSel(){
-    var button = document.getElementById("fromAvToSel");
-    alert(button);
-    button.addEventListener("click", function(){
 
-        let selected = Array.from(AvailableColumn.options)
-            .filter(option => option.selected)
-            .map(option => option.value);
 
-        moveSelectedElement("AvailableColumn", "SelectedColumn");
+// function onClick_fromAvToSel(){
+//     var button = document.getElementById("fromAvToSel");
+//     button.addEventListener("click", function(){
 
-    });
-}
 
-function onClick_fromSelToAv(){
-    var button = document.getElementById("fromSelToAv");
-    alert(button);
-    button.addEventListener("click", function(){
+//         moveSelectedElement("AvailableColumn", "SelectedColumn");
 
-        let selected = Array.from(AvailableColumn.options)
-            .filter(option => option.selected)
-            .map(option => option.value);
+//     });
+// }
 
-        moveSelectedElement("SelectedColumn", "AvailableColumn");
+// function onClick_fromSelToAv(){
+//     var button = document.getElementById("fromSelToAv");
+//     button.addEventListener("click", function(){
 
-    });
-}
+//         // let selected = Array.from(AvailableColumn.options)
+//         //     .filter(option => option.selected)
+//         //     .map(option => option.value);
 
-function onClic_SelectMultipleOptions(){
+//         moveSelectedElement("SelectedColumn", "AvailableColumn");
 
-}
+//     });
+// }
+
+// function onClic_SelectMultipleOptions(){
+
+// }
 
 function moveSelectedElement(sourceID, destinationID) {
 	var source = document.getElementById(sourceID);
 	var dest = document.getElementById(destinationID);
 
 	for(var item = 0; item < source.options.length; item++) {
-
 		if(source.options[item].selected == true) {
 				var option = source.options[item];
 
@@ -53,5 +49,47 @@ function moveSelectedElement(sourceID, destinationID) {
     
 }
 
+function selectedAllOptions(selectID, isSelect) {
+    var myColumn = document.getElementById(selectID);
 
-//window.onload = onClick_fromSelToAv, onClick_fromAvToSel;
+    for (var item = 0; item < myColumn.options.length; item++) {
+        myColumn.options[item].selected = isSelect;
+    }
+}
+
+function moveMyElementToSelected(){
+    if(AvailableColumn.options.length == 0){
+        alert("List is empty!");
+    }
+    moveSelectedElement("AvailableColumn", "SelectedColumn");
+}
+function moveMyElementToAvailable(){
+    if(SelectedColumn.options.length == 0){
+        alert("List is empty!");
+    }
+    moveSelectedElement("SelectedColumn", "AvailableColumn");
+}
+
+function moveAllElementsToSelected(){
+    if(AvailableColumn.options.length == 0){
+        alert("List is empty!");
+    }
+    selectedAllOptions("AvailableColumn", true);
+    moveSelectedElement("AvailableColumn", "SelectedColumn");
+}
+function moveAllElementsToAvailable(){
+    if(SelectedColumn.options.length == 0){
+        alert("List is empty!");
+    }
+    selectedAllOptions("SelectedColumn", true);
+    moveSelectedElement("SelectedColumn", "AvailableColumn");
+}
+
+fromAvToSelButton.addEventListener("click", moveMyElementToSelected);
+fromSelToAvButton.addEventListener("click", moveMyElementToAvailable);
+
+fromAvToSelAllButton.addEventListener("click", moveAllElementsToSelected);
+fromSelToAvALLButton.addEventListener("click", moveAllElementsToAvailable);
+
+window.onload = selectedAllOptions("AvailableColumn", false);
+window.onload = selectedAllOptions("SelectedColumn", false);
