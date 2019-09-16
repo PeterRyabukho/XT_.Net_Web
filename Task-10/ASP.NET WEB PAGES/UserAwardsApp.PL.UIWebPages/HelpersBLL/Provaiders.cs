2bfa86fdad477c4b9f5b3880945b2e6310cc666e
ryabukhopeter.optics <ryabukhopeter.optics@yandex.ru>
2019-09-16 17:11:05 +0400

    refactoring

---
+++ Task-10/ASP.NET WEB PAGES/UserAwardsApp.PL.UIWebPages/HelpersBLL/Provaiders.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UserAwardsApp.BLL.Abstract;
+using UserAwardsApp.DI.Provaiders;
+
+namespace UserAwardsApp.PL.UIWebPages.HelpersBLL
+{
+    public static class Provaiders
+    {
+        public static IUserLogic userLogic = MyProvaider.userLogic;
+        public static IAwardsLogic awardsLogic = MyProvaider.awardsLogic;
+    }
+}
